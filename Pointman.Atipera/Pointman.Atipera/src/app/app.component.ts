daff58f097bd13bb849d6288276db0b2ac0cf34c
MatMic1211 <mat.micz@wp.pl>
2025-06-25 21:41:52 +0200

    Utworzenie przykladowej tabeli i zasilenie jej danymi z serwisu

--- Pointman.Atipera/Pointman.Atipera/src/app/app.component.ts
+++ Pointman.Atipera/Pointman.Atipera/src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ElementService, PeriodicElement } from './services/element.service';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Pointman.Atipera';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['number', 'name', 'weight', 'symbol'];
+  dataSource: PeriodicElement[] = [];
+
+  constructor(private elementService: ElementService) { }
+
+  ngOnInit(): void {
+    this.elementService.getElements().subscribe(data => {
+      this.dataSource = data;
+    });
+  }
 }
